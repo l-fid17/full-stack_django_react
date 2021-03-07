26ec60f76baa932d3e54b798a3f589ded7b8b519
f1d <f1d.pro.luca@gmail.com>
2021-03-07 14:55:37 +0000

    error handling

--- fullstack/frontend/src/components/App.js
+++ fullstack/frontend/src/components/App.js
@@ -2,20 +2,32 @@ import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "../store";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
+import Alert from "./layout/Alert";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
+
+// Alert options
+const alertOptions = {
+  timeout: 3000,
+  position: "top right",
+};
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <Alert />
+            <div className="container">
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
--- fullstack/frontend/src/reducers/index.js
+++ fullstack/frontend/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
 import leads from "./leads";
+import messages from "./messages";
+import errors from "./errors";
 
 export default combineReducers({
   //add reducers here
   leads,
+  errors,
+  messages,
 });
